825e9b8203f6de1315391e90a8430574f162a7fb
Mattias <hej.ma5ttias@gmail.com>
2021-01-07 11:49:32 +0100

    Added comments to Teleserver

--- tele-server/teleServer.js
+++ tele-server/teleServer.js
@@ -4,12 +4,12 @@ var path = require("path")
 var bodyParser = require("body-parser");
 expressWs(app);
 
-
+//Tell body Parser to be a middel ware.
 app.use(bodyParser.urlencoded({extended: true}))
 
 
+//Set the port. Can not be same as the Open MCT PORT.
 const PORT = 5000;
-
 app.listen(PORT, () => {
     console.log("Telecomand server on port " + PORT);
 });
@@ -19,12 +19,15 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/html/telecommand.html"));
   });
 
+// Post method.
 app.post("/", function(req, res){
 
+    // User body-Parser to get data out of form.
     var SendCommand = req.body.command;
-
     console.log(SendCommand);
 
+
+    //Reload the page after post.
     res.sendFile(path.join(__dirname + "/html/telecommand.html"));
 
 });
