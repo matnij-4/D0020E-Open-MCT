f67f47bbb80c9b8b09c3aaa56c9cedcfd48dcee5
Alexander <59938684+Alexhedb@users.noreply.github.com>
2021-01-21 11:44:00 +0100

    saved

--- dictionary-plugin.js
+++ dictionary-plugin.js
@@ -40,63 +40,7 @@ var objectProvider = {
     }
 };
 
-<<<<<<< HEAD
-
-var objectProviderCharg = {
-    get: function (identifier) {
-        return getDictionary('/charging-beacon.json').then( (dictionary) => {
-            if (identifier.key === 'beaconCharg') {
-                return {
-                    identifier: identifier,
-                    name: dictionary.name,
-                    type: 'folder',
-                    location: 'ROOT'
-                };
-            } else {
-                var measurement = dictionary.measurements.filter( (m) => {
-                    return m.key === identifier.key;
-                })[0];
-                
-
-                
-                return {
-                    identifier: identifier,
-                    name: measurement.name,
-                    type: 'telemetry',
-                    telemetry: {
-                        values: measurement.values
-                    },
-                    location: 'charging.beacon:beaconCharg'
-                };
-                
-            }
-        });
-    }
-};
-
-var compositionProviderNom= {
-    appliesTo: function (domainObject) {
-        return domainObject.identifier.namespace === 'nominal.beacon' &&
-               domainObject.type === 'folder';
-    },
-    load: function (domainObject) {
-        return getDictionary('/nominal-beacon.json')
-            .then(function (dictionary) {
-                return dictionary.measurements.map(function (m) {
-                    return {
-                        namespace: 'nominal.beacon',
-                        key: m.key
-                    };
-                });
-            });
-    }
-};
-
-
-var compositionProviderCharg = {
-=======
 var compositionProvider= {
->>>>>>> ec02069b3d02da296c234b0430715a912155a2a6
     appliesTo: function (domainObject) {
         return domainObject.identifier.namespace === NAMESPACE &&
                domainObject.type === 'folder';
