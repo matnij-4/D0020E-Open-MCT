b89dfe60bd1221e1f946b84e264f7106ceee6f02
antstr-8 <antstr-8@student.ltu.se>
2021-02-03 21:37:37 +0100

    Added so we recive data from pre-defined server

--- udpSocket.js
+++ udpSocket.js
@@ -3,16 +3,27 @@ const dgram = require('dgram');
 
 function UDPSocket(){
     //Creates a buffer for a string
-    const message = new Buffer("Some bytes");
+    const key = "1593574862";
+
+    const message = key.toString(16);
     //Creates a udp socket for IPv4
     const client = dgram.createSocket("udp4");
+    const IP = "130.240.14.144";
+    const PORT = 5001;
     //Sends the buffer to a spesifyed ipaddr and port 
     //Then closes the socket. 
-    client.send(message, 0, message.length, 20001, '127.0.0.1');
+    console.log("Sent a message to: ", )
+    client.send(message, 0, message.length, PORT, IP);
+    //Listens to the incoming messages from server
+
+    const recivedFirst = false;
     client.on('message', (msg, rinfo) =>{
-        console.log('Received %d bytes from %s:%d\n',
-              msg.length, rinfo.address, rinfo.port);
+        if(msg.toString() == "AKW"){
+            console.log("Subscribed");
+        }
+        console.log(msg.toString());
     });
+
 }
 //Need to export to be used in openmct
 module.exports = UDPSocket;
