a5fdad64030229a28fde5360bd37fa20a3f99f9f
antstr-8 <antstr-8@student.ltu.se>
2021-03-03 14:34:18 +0100

    Updated connections to sql server

--- history.js
+++ history.js
@@ -1,13 +1,6 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'AeXJxSg/&',
-    database: 'test',
-    multipleStatements: 'true'
-});
-con.connect();
+
 
 function HistoryLoader(){
     // this.getServerData("batt_temp", 0, getFullDate(),function(result){
@@ -19,27 +12,42 @@ function HistoryLoader(){
 };
 
 
-function getFullDate(){
-    var date = new Date();
-    return date.getFullYear()+ "-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-}
+// function getFullDate(){
+//     var date = new Date();
+//     return date.getFullYear()+ "-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+// }
 
-HistoryLoader.prototype.insert = function(key, value){
-    var sql = "INSERT INTO "+ key + " (value, date) VALUES ('"+value+"', '"+getFullDate()+"')";
-    con.query(sql,function(err, result){
-        if(err) throw err;
-        console.log("Line inserted...");
-        return;
+// HistoryLoader.prototype.insert = function(key, value){
+//     var sql = "INSERT INTO "+ key + " (value, date) VALUES ('"+value+"', '"+getFullDate()+"')";
+//     con.query(sql,function(err, result){
+//         if(err) throw err;
+//         console.log("Line inserted...");
+//         return;
+//     });
+// }
+
+function connectServer(){
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'AeXJxSg/&',
+        database: 'test',
+        multipleStatements: 'true'
     });
+    con.connect();
+    return con;
 }
 
 HistoryLoader.prototype.getServerData = function(key, start, stop, callback) {
+    var con = connectServer();
     const startDate = new Date(start);
     const stopDate = new Date(stop);
     const realStart = startDate.getFullYear() + "-"+(startDate.getMonth()+1) + "-" +startDate.getDay()+ " " 
     + startDate.getHours()+":"+startDate.getMinutes()+":"+ startDate.getSeconds();
     const realStop = stopDate.getFullYear() + "-"+(stopDate.getMonth()+1) + "-" +stopDate.getDay()+ " " 
     + stopDate.getHours()+":"+stopDate.getMinutes()+":"+ stopDate.getSeconds();
+
+
     var sql = "SELECT * FROM " + key + " WHERE date > '"+realStart+"' AND date < '"+ realStop+"';";
     
     
@@ -47,6 +55,7 @@ HistoryLoader.prototype.getServerData = function(key, start, stop, callback) {
         if(err) throw err;
         return callback(result);
     });
+    con.end();
 };
 
 module.exports = function(){
